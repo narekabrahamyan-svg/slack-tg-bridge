d7bdec32773321ec18af1f76775eb528757d42c5
narekabrahamyan-svg <narek.abrahamyan@newguys.io>
2026-04-09 16:23:05 +0400

    Update server.js

--- server.js
+++ server.js
@@ -11,27 +11,19 @@ app.post("/slack", async (req, res) => {
   if (req.body.type === "url_verification") {
     return res.json({ challenge: req.body.challenge });
   }
-
   const event = req.body.event;
   if (!event || event.type !== "message" || event.bot_id) {
     return res.sendStatus(200);
   }
-
   const text = event.text || "(empty message)";
   const user = event.user || "unknown";
   const msg = "Slack message\nUser: " + user + "\n" + text;
-
   await fetch("https://api.telegram.org/bot" + TG_TOKEN + "/sendMessage", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ chat_id: TG_CHAT_ID, text: msg }),
+    body: JSON.stringify({ chat_id: TG_CHAT_ID, text: msg })
   });
-
   res.sendStatus(200);
 });
 
 app.listen(3000, () => console.log("Bridge running on port 3000"));
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => console.log("Bridge running on port 3000"));}
